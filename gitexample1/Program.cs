22f4865dcf8d5436bbf09d30c2addeb5b480d64b
mmosier-web <mmosier5@gmail.com>
2019-09-04 11:42:19 -0400

    first commit

---
+++ gitexample1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace gitexample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        { 
+            string firstName = "Mike";
+            string lastName = "mosier";
+            int age = 27;
+
+            int puttingOnSomeYears = 33;
+
+            int oldAge = age + puttingOnSomeYears;
+
+            Console.WriteLine(firstName + " " + lastName);
+
+
+
+            Console.WriteLine("I am " + age + " years old");
+
+
+
+            Console.WriteLine("I'm really glad I'm not "+oldAge+"years old");
+               
+        }
+    }
+}
